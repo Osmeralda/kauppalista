1761ba900f5f2e9cc3876f998f451901b4dd8808
osmeralda <oskari.kurtti@student.oulu.fi>
2022-09-05 10:53:21 +0300

    toimiva versio joka on deployattu ja toimii tästä lähtien kodin kauppalistana

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { HashRouter as Router, Route, Routes} from "react-router-dom";
 import Login from "./Login";
 import Register from "./Register";
 import Reset from "./Reset";
